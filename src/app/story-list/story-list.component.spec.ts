201882ed0c7e460288219bab12e49ae182957e58
PardeepRsystems <153589101+PardeepRsystems@users.noreply.github.com>
2023-12-13 18:22:48 +0530

    Update story-list.component.spec.ts

--- src/app/story-list/story-list.component.spec.ts
+++ src/app/story-list/story-list.component.spec.ts
@@ -45,7 +45,7 @@ describe('StoryListComponent', () => {
     expect(component.currentPage).toEqual(1);
     expect(component.itemsPerPage).toEqual(50);
     expect(component.totalItems).toEqual(0);
-    expect(component.tableSize).toEqual([5, 10, 15, 20]);
+    expect(component.tableSize).toEqual([50, 100, 150, 200]);
     expect(component.news).toEqual([]);
     expect(component.searchString).toEqual('');
     expect(component.pagingConfig).toEqual({
